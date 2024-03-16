1f31c538fe18e275b53d71e2b0946663e7abed6c
ivannstu24 <nstu-git@mail.ru>
2024-03-17 00:32:22 +0700

    fetch: add Beta file

---
+++ rectangle/main.js
@@ -0,0 +1,37 @@
+    // Функция для вычисления периметра прямоугольника
+function calculateRectanglePerimeter(width, height) {
+    return 2 * (width + height);
+}
+
+// Функция для вычисления площади прямоугольника
+function calculateRectangleArea(width, height) {
+    return width * height;
+}
+
+// Функция для вычисления длины диагонали прямоугольника
+function calculateRectangleDiagonal(width, height) {
+    return Math.sqrt(width * width + height * height);
+}
+
+// Получаем ширину и высоту от пользователя
+const widthInput = parseFloat(prompt("Введите ширину прямоугольника:"));
+const heightInput = parseFloat(prompt("Введите высоту прямоугольника:"));
+
+// Проверяем, что пользователь ввел корректные значения
+if (!isNaN(widthInput) && !isNaN(heightInput) && widthInput > 0 && heightInput > 0) {
+    const width = widthInput;
+    const height = heightInput;
+
+    const outputElement = document.querySelector(".output");
+
+    outputElement.innerHTML = `
+        <p>Периметр прямоугольника: ${calculateRectanglePerimeter(width, height)}</p>
+        <p>Площадь прямоугольника: ${calculateRectangleArea(width, height)}</p>
+        <p>Длина диагонали прямоугольника: ${calculateRectangleDiagonal(width, height)}</p>`;
+
+    console.log("Периметр прямоугольника:", calculateRectanglePerimeter(width, height));
+    console.log("Площадь прямоугольника:", calculateRectangleArea(width, height));
+    console.log("Длина диагонали прямоугольника:", calculateRectangleDiagonal(width, height));
+} else {
+    console.log("Пожалуйста, введите корректные положительные числовые значения для ширины и высоты прямоугольника.");
+}
