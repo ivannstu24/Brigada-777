4dd94d7fc4a39654f646bd8fced07d876e446155
Anastasiastsv <101170767+Anastasiastsv@users.noreply.github.com>
2024-03-17 22:18:39 +0700

    Update and rename mycod.js to circle.js

---
+++ circle.js
@@ -0,0 +1,33 @@
+// Подключаем модуль readline для работы с вводом/выводом
+const readline = require('readline');
+
+
+// Создаем интерфейс для чтения из стандартного ввода и записи в стандартный вывод
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+
+//Функция для вычисления параметров окружности
+function calculateCircleParameters(radius, alpha)
+{
+  const circleLength = 2 * Math.PI * radius; //вычисление длины окружности
+  const circleArea = Math.PI * radius * radius; //вычисление площади круга
+  const circleSector = (Math.PI * radius * radius * alpha)/360; //вычисление площади кругового сектора
+  return { circleLength, circleArea, circleSector };
+}
+
+
+rl.question('Введите радиус окружности : ', (radius) => {
+  rl.question('Введите градусную меру угла дуги, на которую опирается сектор : ', (alpha) => {
+// вычисляем параметры окружности
+    const circle = calculateCircleParameters(parseFloat(radius), parseFloat(alpha));
+    console.log(`Длина окружности: ${circle.cicleLength}`);
+    console.log(`Площадь круга: ${circle.circleArea}`);
+    console.log(`Площадь кругового сектора: ${circle.circleSector}`);
+
+    rl.close();
+  });
+});
+
